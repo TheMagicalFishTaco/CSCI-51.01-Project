caf1a952d8ec870ba793a3348d2d923bb3c6161f
Lance Co <lance.co@obf.ateneo.edu>
2021-05-16 14:44:49 +0800

    Updated base code, works now

--- CSCI 51.01 Project/CSCI 51.01 Project/CSCI 51.01 Project.cpp
+++ CSCI 51.01 Project/CSCI 51.01 Project/CSCI 51.01 Project.cpp
@@ -6,20 +6,22 @@
 using namespace std;
 
 int main()
-{
+{    
+    vector<double> arrivalTime(0);
+    vector<double> burstTime(0);
+    vector<double> priority(0);
     int numTestCase, numProcess, inputArrival, inputBurst, inputPriority;
     string scheduleAlgorithm;
 
     cin >> numTestCase;
     cin >> numProcess >> scheduleAlgorithm;
 
-    vector<double> arrivalTime(numProcess);
-    vector<double> burstTime(numProcess);
-    vector<double> priority(numProcess);
+
 
     for (int i = 0; i < numTestCase; i++)
     {
-        for (int o = 0; i < numProcess; o++)
+        //input loop for the processes, stores them into vectors
+        for (int o = 0; o < numProcess; o++)
         {
             cin >> inputArrival >> inputBurst >> inputPriority;
             arrivalTime.push_back(inputArrival);
@@ -29,7 +31,12 @@ int main()
 
         if (scheduleAlgorithm == "FCFS")
         {
-
+            //just made this to test if the inputs are being recieved
+            cout << "schedule algorithm: " << scheduleAlgorithm << "\n";
+            for (int q = 0; q < arrivalTime.size(); q++)
+            {
+                cout << "process " << q << ": " << arrivalTime.at(q) << " " << burstTime.at(q) << " " << priority.at(q) << "\n";
+            }
         }
         else if (scheduleAlgorithm == "SJF")
         {
