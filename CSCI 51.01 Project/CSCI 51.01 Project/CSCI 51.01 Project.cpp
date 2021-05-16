f9cc243ff60f39badf167f50b7b4400bdbbc90ff
Lance Co <lance.co@obf.ateneo.edu>
2021-05-16 14:30:11 +0800

    Added base code

--- CSCI 51.01 Project/CSCI 51.01 Project/CSCI 51.01 Project.cpp
+++ CSCI 51.01 Project/CSCI 51.01 Project/CSCI 51.01 Project.cpp
@@ -2,19 +2,52 @@
 //
 
 #include <iostream>
+#include <vector>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int numTestCase, numProcess, inputArrival, inputBurst, inputPriority;
+    string scheduleAlgorithm;
+
+    cin >> numTestCase;
+    cin >> numProcess >> scheduleAlgorithm;
+
+    vector<double> arrivalTime(numProcess);
+    vector<double> burstTime(numProcess);
+    vector<double> priority(numProcess);
+
+    for (int i = 0; i < numTestCase; i++)
+    {
+        for (int o = 0; i < numProcess; o++)
+        {
+            cin >> inputArrival >> inputBurst >> inputPriority;
+            arrivalTime.push_back(inputArrival);
+            burstTime.push_back(inputBurst);
+            priority.push_back(inputPriority);
+        }
+
+        if (scheduleAlgorithm == "FCFS")
+        {
+
+        }
+        else if (scheduleAlgorithm == "SJF")
+        {
+
+        }
+        else if (scheduleAlgorithm == "SRTF")
+        {
+
+        }
+        else if (scheduleAlgorithm == "P")
+        {
+
+        }
+        else if (scheduleAlgorithm == "RR")
+        {
+
+        }
+    }
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
