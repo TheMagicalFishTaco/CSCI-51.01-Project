72c65c2f60059a91df63f68d4e302086d9905a3d
Lance Co <lance.co@obf.ateneo.edu>
2021-05-17 13:12:47 +0800

    Fixed I/O to use text files

--- CSCI 51.01 Project/CSCI 51.01 Project/CSCI 51.01 Project.cpp
+++ CSCI 51.01 Project/CSCI 51.01 Project/CSCI 51.01 Project.cpp
@@ -2,24 +2,33 @@
 //
 
 #include <iostream>
+#include <fstream>
 #include <vector>
 using namespace std;
 
 int main()
 {    
-    vector<double> arrivalTime(0);
-    vector<double> burstTime(0);
-    vector<double> priority(0);
+    vector<int> arrivalTime(0);
+    vector<int> burstTime(0);
+    vector<int> priority(0);
     int numTestCase, numProcess, inputArrival, inputBurst, inputPriority;
     string scheduleAlgorithm;
 
+    //creates the output text file
+    ofstream outputFile("testOutput.txt");
+
     cin >> numTestCase;
-    cin >> numProcess >> scheduleAlgorithm;
+
 
 
 
     for (int i = 0; i < numTestCase; i++)
-    {
+    {    
+        cin >> numProcess >> scheduleAlgorithm;
+        arrivalTime.clear();
+        burstTime.clear();
+        priority.clear();
+
         //input loop for the processes, stores them into vectors
         for (int o = 0; o < numProcess; o++)
         {
@@ -31,11 +40,10 @@ int main()
 
         if (scheduleAlgorithm == "FCFS")
         {
-            //just made this to test if the inputs are being recieved
-            cout << "schedule algorithm: " << scheduleAlgorithm << "\n";
+            outputFile << "schedule algorithm: " << scheduleAlgorithm << "\n";
             for (int q = 0; q < arrivalTime.size(); q++)
             {
-                cout << "process " << q << ": " << arrivalTime.at(q) << " " << burstTime.at(q) << " " << priority.at(q) << "\n";
+                outputFile << "process " << q+1 << ": " << arrivalTime.at(q) << " " << burstTime.at(q) << " " << priority.at(q) << "\n";
             }
         }
         else if (scheduleAlgorithm == "SJF")
