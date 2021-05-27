fa0c55b5c25a099a6e08958d05e032622dcca79d
minguitHub <suminguitsylvane@gmail.com>
2021-05-28 05:34:05 +0800

    Major changes
    1) finished the FCFS 2)changes the struct to include the metrics as members for easier referencing in the Output Logs 3)I'm also uploading a sources.txt so you guys can just add to there 4)I was able to complete the Output Log and it shoudl work for all types of scheduling just make sure you calculate your metrics correctly 5) also the testInput and testOutput are for FCFS only so pls disregard

--- CSCI 51.01 Project/CSCI 51.01 Project/CSCI 51.01 Project.cpp
+++ CSCI 51.01 Project/CSCI 51.01 Project/CSCI 51.01 Project.cpp
@@ -1,4 +1,6 @@
 // CSCI 51.01 Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// ignore line 3 this is for me only - Syl
+// run with C:\Users\Syl\Documents\AASylvane\"CSCI 51.01"\CSCI-51.01-Project\"CSCI 51.01 Project"\"CSCI 51.01 Project"\"CSCI 51.01 Project" < testInput.txt
 
 #include <iostream>
 #include <fstream>
@@ -8,17 +10,29 @@ using namespace std;
 
 struct Process
 {
-    int arrivalTime, burstTime, priority;
-    bool operator < (const Process& rhs) const 
-    {
-        return arrivalTime < rhs.arrivalTime;
+    int arrivalTime, burstTime, priority, PID, waitingTime, turnaroundTime, responseTime;
+
+    Process() {
+        arrivalTime = 0;
+        burstTime = 0;
+        priority = 0;
+        waitingTime = 0;
+        turnaroundTime = 0;
+        responseTime = 0;
     }
 };
+
+bool compareByArrivalTime (const Process &a, const Process &b) {
+    return a.arrivalTime < b.arrivalTime;
+}
+
+bool compareByPID (const Process &a, const Process &b) {
+    return a.PID < b.PID;
+}
 
 int main()
 {    
     vector<Process> processVector;
-
     int numTestCase, inputArrival, inputBurst, inputPriority;
     float time, numProcess;
     string scheduleAlgorithm;
@@ -38,39 +52,53 @@ int main()
         for (int o = 0; o < numProcess; o++)
         {
             cin >> inputArrival >> inputBurst >> inputPriority;
-            processVector.push_back({ inputArrival, inputBurst, inputPriority });
+            Process p;
+            p.arrivalTime = inputArrival;
+            p.burstTime = inputBurst;
+            p.priority = inputPriority;
+            p.PID = o + 1;
+            processVector.push_back(p);
         }
 
         if (scheduleAlgorithm == "FCFS")
         {
-            outputFile << "schedule algorithm: " << scheduleAlgorithm << "\n";
-            for (int q = 0; q < processVector.size(); q++)
-            {
+            outputFile << i+1 << " " << scheduleAlgorithm << "\n";
+            sort(processVector.begin(), processVector.end(), compareByArrivalTime);
 
-            }
-        }
-        else if (scheduleAlgorithm == "SJF")
-        {
-            outputFile << "schedule algorithm: " << scheduleAlgorithm << "\n";
-            for (int q = 0; q < processVector.size(); q++)
-            {
+            for (auto j = processVector.begin(); j != processVector.end(); ++j) {
+                // this is to fast forward the time in case processes haven't arrived yet
+                if (time < processVector[distance(processVector.begin(), j)].arrivalTime) {
+                    time = processVector[distance(processVector.begin(), j)].arrivalTime;
+                }
 
-            }
-        }
-        else if (scheduleAlgorithm == "SRTF")
-        {
-            outputFile << "schedule algorithm: " << scheduleAlgorithm << "\n";
-            for (int q = 0; q < processVector.size(); q++)
-            {
+                //output
+                outputFile << time << " " << processVector[distance(processVector.begin(), j)].PID << " " << processVector[distance(processVector.begin(), j)].burstTime << "X" << "\n";
+                
+                // adds burst Time to the time elapsed so far
+                time += processVector[distance(processVector.begin(), j)].burstTime;
 
+                /// Setting the metrics
+                // turnaroundTime -- equal to burstTime always
+                processVector[distance(processVector.begin(), j)].turnaroundTime = processVector[distance(processVector.begin(), j)].burstTime;
+                
+                // waitingTime -- for FCFS this is just equal to the Response Time
+                // responseTime -- the only case where there is waiting time is if a new process arrives while old one is not yet done
+                for (auto k = processVector.begin(); k != processVector.end(); ++k) {
+                    if (processVector[distance(processVector.begin(), k)].arrivalTime < time && k > j) {
+                        processVector[distance(processVector.begin(), k)].waitingTime = time - processVector[distance(processVector.begin(), k)].arrivalTime;
+                        processVector[distance(processVector.begin(), k)].responseTime = processVector[distance(processVector.begin(), k)].waitingTime;
+                    }
+                }     
             }
+
         }
+
         //Lance's part
         else if (scheduleAlgorithm == "P")
         {
             outputFile << i+1 << " " << scheduleAlgorithm << "\n";
             //sorts the stack by their arrival time
-            sort(processVector.begin(), processVector.end());
+            sort(processVector.begin(), processVector.end(), compareByArrivalTime);
             //the final process is separated, i don't know why but it doesn't like it if I "process" it within this while loop
             while (processVector.size() > 1)
             {
@@ -78,7 +106,7 @@ int main()
                 //if either condition's good, it will finish the current process completely
                 if ((time + processVector[0].burstTime) < processVector[1].arrivalTime || processVector[0].priority < processVector[1].priority)
                 {
-                    outputFile << time << " " << processVector[0].burstTime << " " << processVector[0].priority <<"X" << "\n";
+                    outputFile << time << " " << processVector[0].burstTime << " " << processVector[0].priority << "X\n";
                     time = time + processVector[0].burstTime;
                     processVector.erase(processVector.begin());
                 }
@@ -115,12 +143,41 @@ int main()
 
             }
         }
-        //Output logs, add the others later
-        outputFile << "Total Time Elapsed: " << time <<" ns"<<"\n";
-        outputFile << "Total CPU Burst Time: " << time << " ns"<<"\n";
-        outputFile << "CPU Utilization: " << "\n";
-        outputFile << "Throughput: " << numProcess / time << " processes/ns"<< "\n";
-    }
 
+        int summationBT = 0, summationWT = 0, summationTT = 0, summationRT = 0;
+
+        // re-sorting everything just to make life easier when displaying the metrics for each process
+        sort(processVector.begin(), processVector.end(), compareByPID);
+
+        //gets the sum of the metrics, which will be later divided by the vector size to get average
+        for (auto i = processVector.begin(); i != processVector.end(); ++i) {
+            summationBT += (*i).burstTime;
+            summationWT += (*i).waitingTime;
+            summationTT += (*i).turnaroundTime;
+            summationRT += (*i).responseTime;
+        }
+
+        // Output logs
+        outputFile << "Total Time Elapsed: " << time <<" ns"<<"\n";
+        outputFile << "Total CPU Burst Time: " << summationBT << " ns"<<"\n";
+        outputFile << "CPU Utilization: " << (int) (summationBT / time * 100) << "%\n";
+        outputFile << "Throughput: " << (float) processVector.size() / time << " processes/ns" << "\n";
+        outputFile << "Waiting times:\n";
+        for (int i = 0; i < processVector.size(); i++) {
+            outputFile << " Process " << i+1 << ": " << processVector[i].waitingTime << "ns\n";
+        }
+        outputFile << "Average waiting time: " << (float) summationWT / processVector.size() << "ns\n";
+        outputFile << "Turnaround times:\n";
+        for (int i = 0; i < processVector.size(); i++) {
+            outputFile << " Process " << i+1 << ": " << processVector[i].turnaroundTime << "ns\n";
+        }
+        outputFile << "Average turnaround time: " << (float) summationTT / processVector.size() << "ns\n";
+        outputFile << "Response times:\n";
+        for (int i = 0; i < processVector.size(); i++) {
+            outputFile << " Process " << i+1 << ": " << processVector[i].responseTime << "ns\n";
+        }
+        outputFile << "Average response time: " << (float) summationRT / processVector.size() << "ns\n";
+    }
+    return 0;
 }
 
